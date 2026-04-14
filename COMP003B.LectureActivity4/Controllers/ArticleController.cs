039c97ea942e7ac6cf57b86a8d423d653caf1bee
cbig3 <romanvillagran386@gmail.com>
2026-04-13 19:04:51 -0700

    added the controller file and everything in it

---
+++ COMP003B.LectureActivity4/Controllers/ArticleController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using COMP003B.LectureActivity4.Models;
+
+namespace COMP003B.LectureActivity4.Models.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public IActionResult Index()
+        {
+            var articles = new List<Article>()
+            {
+                new Article {Id = 1, Title = "Intro to Razor", Summary = "Learn the basics of Razor syntax"},
+                new Article {Id = 2, Title = "Layout and partials", Summary = "Understand Razor layout and reuseable views"},
+            };
+            return View(articles);
+
+        }
+        public IActionResult Details (int id)
+        {
+            var article = new Article { Id = id, Title = $"Article {id}", Summary = "Detailed content coming soon" };
+            return View(article);
+        }
+    }
+}
